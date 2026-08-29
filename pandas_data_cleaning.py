7c2e41b68d03a3b4048e5e7be06abb4bbe8c8940
Athmik A <athmik27@gmail.com>
2026-08-29 16:47:25 +0530

    Uncomment data cleaning steps in pandas script

--- pandas_data_cleaning.py
+++ pandas_data_cleaning.py
@@ -7,29 +7,29 @@ df=pd.read_csv("sample.csv")
 # print(df)
 
 #(1) drop the irrelevent columns
-# df=df.drop(columns=["Legendary"])
-# print(df)
+df=df.drop(columns=["Legendary"])
+print(df)
 
-# df=df.drop(columns=["Legendary","No"])# this removes No and Legendary
-# print(df)
+df=df.drop(columns=["Legendary","No"])# this removes No and Legendary
+print(df)
 
 #(2) handel missing data
-# df=df.dropna(subset=["Type2"])
-# print(df) 
+df=df.dropna(subset=["Type2"])
+print(df) 
 #dropna=drop not available removes rows containing missing (NaN) values.
 #subset=["Type2"] → Tells pandas Only check the Type2 column for missing values."
 
 #drop()      → I tell Pandas WHAT to drop
 #dropna()    → Pandas finds missing values and drops them
-# print(df.to_string())
+print(df.to_string())
 
 #fillna =fill not available this fills the not available columns with 'None'
-# df=df.fillna({"Type2": "None"})
-# print(df.to_string())
+#fillna =fill not available this fills the not available columns with 'None'
+df = df.fillna({"Type2": "None"})
 
 #(3) fix inconsistent values
-# df["Type"]=df["Type1"].replace({"Grass":"GRASS"})
-# print(df.to_string()) # this replaces the Grass  to GRASS
+df["Type"]=df["Type1"].replace({"Grass":"GRASS"})
+print(df.to_string()) # this replaces the Grass  to GRASS
 
 #(4) standardize text
 df["Name"]=df["Name"].str.lower() # this gives all the names in lower case
