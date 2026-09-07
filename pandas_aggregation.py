d163dcd4da52e1bd8e15bc54aa8fd1374c17f04c
Athmik <athmik@Athmiks-MacBook-Neo.local>
2026-09-07 23:40:03 +0530

    added updated modules

--- pandas_aggregation.py
+++ pandas_aggregation.py
@@ -4,28 +4,48 @@ import pandas as pd
 
 df=pd.read_csv("sample.csv")
 print(df)
-#mean() func is usually used to find mean/average for numeric data column's only
+#   mean() func is usually used to find mean/average for numeric data column's only
 
 # these agregate func is for whole data frame
-# print(df.mean(numeric_only=True))
-# print(df.sum(numeric_only=True))
-# print(df.min(numeric_only=True))
-# print(df.max(numeric_only=True))
-# print(df.count()) # for count we need not pass any argument
-                  # count() wont include any null values
+print(df.mean(numeric_only=True))
+print(df.sum(numeric_only=True))
+print(df.min(numeric_only=True))
+print(df.max(numeric_only=True)) # numeric_only=True means peform operations on that column which has numerical values.so any column that has text data are being ignored and only numerical data type is used.
+# and we use this numeric_only=True because for 'text' performing operations like mean,sum is not valid.
+print(df.count()) # for count we need not pass any argument
+                  # count() wont include any null values and counts the number of non-empty  values in each column's
 
 # agregate func for single column
-# print(df["Height"].mean()) # inside [] just put the column name
-# print(df["Weight"].sum())
-# print(df["Height"].min())
-# print(df["Weight"].max())
-# print(df['Height'].count())
+# syntax:
+# df["column_name"].aggregate_function()
+print(df["Height"].mean()) # inside [] just put the column name
+print(df["Weight"].sum())
+print(df["Height"].min())
+print(df["Weight"].max())
+print(df['Height'].count())
 
 # we now group the data frame but use of groupby() keyword
-# group=df.groupby("Type1")
-# print(group) #we get the below warning because for groupby() we must tell it what to do with each group.
+group=df.groupby("Type1")
+print(group) #we get the below warning because for groupby() we must tell it what to do with each group.
 #<pandas.api.typing.DataFrameGroupBy object at 0x10880fe00>
-# so we write
+# so we write print(group["Height"].sum())
+# Separate my DataFrame into groups according to the Type1 column."
+
 group=df.groupby("Type1")
 print(group["Height"].mean())
 print(group["Height"].sum())
+
+
+# df["Height"].mean()      # Average
+# df["Height"].sum()       # Total
+# df["Height"].min()       # Minimum
+# df["Height"].max()       # Maximum
+# df["Height"].count()     # Number of non-empty values
+# df["Height"].median()    # Middle value
+# df["Height"].std()       # Standard deviation
+# df["Height"].var()       # Variance
+
+# agg() function:
+# here we can perform multiple aggregate functions at once.
+
+print(df["Height"].agg(["mean", "min", "max", "sum"]))
--- pandas_data_cleaning.py
+++ pandas_data_cleaning.py
@@ -4,32 +4,35 @@
 import pandas as pd
 
 df=pd.read_csv("sample.csv")
-# print(df)
+print(df)
 
 #(1) drop the irrelevent columns
-# df=df.drop(columns=["Legendary"])
-# print(df)
+df=df.drop(columns=["Legendary"])
+print(df)
 
-# df=df.drop(columns=["Legendary","No"])# this removes No and Legendary
-# print(df)
+df=df.drop(columns=["Legendary","No"])# this removes No and Legendary columms.
+print(df)
 
 #(2) handel missing data
-# df=df.dropna(subset=["Type2"])
-# print(df) 
-#dropna=drop not available removes rows containing missing (NaN) values.
+df=df.dropna(subset=["Type2"]) # if no subset dropna check's the entire data table.
+print(df) 
+#dropna=drop not available removes rows containing missing values.
 #subset=["Type2"] → Tells pandas Only check the Type2 column for missing values."
 
 #drop()      → I tell Pandas WHAT to drop
 #dropna()    → Pandas finds missing values and drops them
-# print(df.to_string())
+print(df.to_string()) # print(df.to_string())
+# asks Pandas to convert the whole DataFrame into a string, so you can see all the data.
+
+
 
 #fillna =fill not available this fills the not available columns with 'None'
-# df=df.fillna({"Type2": "None"})
-# print(df.to_string())
+df=df.fillna({"Type2": "None"}) # df.fillna({column: replacement_value})
+print(df.to_string())
 
 #(3) fix inconsistent values
-# df["Type"]=df["Type1"].replace({"Grass":"GRASS"})
-# print(df.to_string()) # this replaces the Grass  to GRASS
+df["Type"]=df["Type1"].replace({"Grass":"GRASS"}) # df[Type] creates an new column that stores the updated value.
+print(df.to_string()) # this replaces the Grass  to GRASS
 
 #(4) standardize text
 df["Name"]=df["Name"].str.lower() # this gives all the names in lower case
@@ -42,3 +45,7 @@ print(df.to_string())
 #(6) remove duplicate datas
 df=df.drop_duplicates()
 print(df.to_string())
+
+df["Name"].str.lower()      # lowercase
+df["Name"].str.upper()      # uppercase
+df["Name"].str.strip()      # remove extra spaces.
