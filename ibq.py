f8cac1bbd51dbf29b7ce840b9f82308c0c9bbcc8
Athmik <athmik@Athmiks-MacBook-Neo.local>
2026-09-18 17:57:24 +0530

    Added Commit

--- ibq.py
+++ ibq.py
@@ -30,4 +30,36 @@ import pandas as pd
 # df=pd.read_csv("sample.csv")
 # print(df["Type1"].value_counts())
 
-# Find the maximum Weight among all Pokémon.
+#output predection
+# import pandas as pd
+# s = pd.Series([10, 20, 30, 40])
+# print(s)
+
+# import pandas as pd
+# s = pd.Series([100, 200, 300], index=["A", "B", "C"])
+# # print(s["B"])
+
+# import pandas as pd
+# s = pd.Series([10, 20, 30, 40])
+# print(s[2])
+
+# import pandas as pd
+# s = pd.Series(50, index=["A", "B", "C"])
+# print(s)
+
+# import pandas as pd
+# s = pd.Series([10, 20, 30, 40])
+# print(s[1:3])
+
+# import pandas as pd
+# s = pd.Series([10, 20, 30], index=["A", "B", "C"])
+# print(s.index)
+
+import pandas as pd
+s = pd.Series([10, 20, 30], index=["A", "B", "C"])
+print(s.values) # s.index    # gives labels
+#                s.values   # gives values
+
+# Create a Pandas Series containing:
+# 10, 20, 30, 40, 50
+# Then print the Series.
