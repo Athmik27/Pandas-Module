66e6d93186de1a72e68d71fe30a07dff57d5d7a4
Athmik A <athmik27@gmail.com>
2026-09-20 20:42:33 +0530

    Uncomment code for Pokémon data analysis

--- ibq.py
+++ ibq.py
@@ -13,18 +13,18 @@ print(pokemon_select)
 # # Syntax: df[(df["Column1"] > value) & (df["Column2"] > value)]
 
 # # Display all Pokémon whose Type1 is either "Fire" OR "Water".
-# df = pd.read_csv("sample.csv")
-# pokemon_select = df[(df["Type1"] == 'Fire') | (df["Type1"] == 'Water')]
-# print(pokemon_select)
+df = pd.read_csv("sample.csv")
+pokemon_select = df[(df["Type1"] == 'Fire') | (df["Type1"] == 'Water')]
+print(pokemon_select)
 
 #  Display the 5 heaviest Pokémon, with the heaviest one appearing first very imp
-# df = pd.read_csv("sample.csv")
-# print(df.sort_values("Weight", ascending=False).head(5))
+df = pd.read_csv("sample.csv")
+print(df.sort_values("Weight", ascending=False).head(5))
 
 # Find the average Weight of Pokémon for each Type1.
-# df = pd.read_csv("sample.csv")
-# group=(df.groupby("Type1"))
-# print(group['Weight'].mean())
+df = pd.read_csv("sample.csv")
+group=(df.groupby("Type1"))
+print(group['Weight'].mean())
 
 # # Find how many Pokémon belong to each Type1
 # df=pd.read_csv("sample.csv")
