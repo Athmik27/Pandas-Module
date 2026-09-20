c34aa9c9e23eb9fbf88235bd791a67f9a8adda4d
Athmik A <athmik27@gmail.com>
2026-09-20 20:44:11 +0530

    Uncomment series creation and indexing example

--- ibq.py
+++ ibq.py
@@ -36,8 +36,8 @@ s = pd.Series([10, 20, 30, 40])
 print(s)
 
 # import pandas as pd
-# s = pd.Series([100, 200, 300], index=["A", "B", "C"])
-# # print(s["B"])
+s = pd.Series([100, 200, 300], index=["A", "B", "C"])
+# print(s["B"])
 
 # import pandas as pd
 # s = pd.Series([10, 20, 30, 40])
